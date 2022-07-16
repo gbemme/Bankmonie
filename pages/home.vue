038b23365c749e20239b4f108bf6fb6fc2d09362
Ibrahim Ajao <ajaoibra93@gmail.com>
2022-07-16 20:08:38 +0100

    WhyBankMonie.vue

--- pages/home.vue
+++ pages/home.vue
@@ -2,15 +2,17 @@
 <v-app>
   <HeroPage/>
   <Upgrade/>
+  <WhyBankMonie/>
 </v-app>
 </template>
 
 <script>
 import HeroPage from "~/components/landingPage/HeroPage";
 import Upgrade from "../components/landingPage/Upgrade";
+import WhyBankMonie from "../components/WhyBankMonie";
 export default {
   name: "home",
-  components: {Upgrade, HeroPage}
+  components: {WhyBankMonie, Upgrade, HeroPage}
 }
 </script>
 
